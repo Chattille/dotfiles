52f763e2863ba3108c5bacedd2e6ff4b53b57673
Chattille <1023301484@qq.com>
2024-07-05 20:52:50 +0800

    chore(nvim): format code

--- nvim/lua/conf/lazy.lua
+++ nvim/lua/conf/lazy.lua
@@ -1,18 +1,25 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 vim.opt.rtp:prepend(lazypath)
 
 -- bootstrap
 if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    local out = vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        lazyrepo,
+        lazypath,
+    }
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-	    { out, 'WarningMsg' },
-	    { '\nPress any key to exit...' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
         }, true, {})
-	vim.fn.getchar()
-	os.exit(1)
+        vim.fn.getchar()
+        os.exit(1)
     end
 end
 
