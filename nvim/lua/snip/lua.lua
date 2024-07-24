8e390655c2fb605684cbdcc19a68ad729fc15497
Chattille <1023301484@qq.com>
2024-07-24 16:13:45 +0800

    feat(nvim/snip): add snippets for Lua files

--- nvim/lua/snip/lua.lua
+++ nvim/lua/snip/lua.lua
@@ -1,5 +1,17 @@
 local snippets = {
     s('vp', fmt([[vim.print({})]], { i(1) })),
+    s(
+        'new',
+        fmt(
+            [[
+            function {1}.new()
+                return setmetatable({}, {{ __index = {1} }})
+            end
+            ]],
+            { i(1), i(2, '{}') },
+            { repeat_duplicates = true }
+        )
+    ),
 }
 
 return snippets
