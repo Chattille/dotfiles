362e1d4e392a30c9d7c1884faab605c1dffe7bfd
Chattille <1023301484@qq.com>
2026-03-19 18:28:54 +0800

    feat(nvim): improve aesthetics for telescope

--- nvim/lua/plug/search.lua
+++ nvim/lua/plug/search.lua
@@ -45,7 +45,7 @@ return {
     {
         'nvim-telescope/telescope-fzf-native.nvim',
         dependencies = { 'nvim-telescope/telescope.nvim' },
-        build = 'make',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install',
     },
 
     {
