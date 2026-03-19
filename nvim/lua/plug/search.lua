ccefffd66ac96e7d75121cb0f29659471045343f
Chattille <1023301484@qq.com>
2026-03-19 15:21:44 +0800

    fix(nvim): fix telescope.nvim

--- nvim/lua/plug/search.lua
+++ nvim/lua/plug/search.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
+        version = '*',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'prochri/telescope-all-recent.nvim',
