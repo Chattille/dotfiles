05b11f2b3605d15961cd1c4be7285019e8e1dd08
Chattille <1023301484@qq.com>
2026-03-28 01:27:37 +0800

    fix(nvim): fix broken nvim-treesitter-textobjects

--- nvim/lua/plug/syntax.lua
+++ nvim/lua/plug/syntax.lua
@@ -2,154 +2,124 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        config = function(_, opts)
+        config = function()
             require 'conf.treesitter'
-            require('nvim-treesitter').setup(opts)
         end,
-        opts = {
-            highlight = { enable = true },
-            indent = {
-                enable = true,
-                disable = { 'html' },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    disable = {
-                        'css',
-                        'dockerfile',
-                        'json',
-                        'jsonc',
-                        'make',
-                        'markdown_inline',
-                        'query',
-                        'scss',
-                        'vimdoc',
-                        'xml',
-                        'yaml',
-                    },
-                    keymaps = {
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                    },
-                    selection_modes = {
-                        ['@function.inner'] = 'v',
-                        ['@function.outer'] = 'V',
-                        ['@class.inner'] = 'v',
-                        ['@class.outer'] = 'V',
-                    },
-                    include_surrounding_whitespace = true,
-                },
-                move = {
-                    enable = true,
-                    disable = {
-                        'css',
-                        'dockerfile',
-                        'json',
-                        'jsonc',
-                        'make',
-                        'markdown_inline',
-                        'query',
-                        'scss',
-                        'vimdoc',
-                        'xml',
-                        'yaml',
-                    },
-                    goto_next_start = {
-                        [']f'] = '@function.outer',
-                        [']c'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']F'] = '@function.outer',
-                        [']C'] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[f'] = '@function.outer',
-                        ['[c'] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[F'] = '@function.outer',
-                        ['[C'] = '@class.outer',
-                    },
-                },
-                lsp_interop = {
-                    enable = true,
-                    disable = {
-                        'bash',
-                        'css',
-                        'dockerfile',
-                        'html',
-                        'json',
-                        'jsonc',
-                        'make',
-                        'markdown',
-                        'markdown_inline',
-                        'query',
-                        'scss',
-                        'vim',
-                        'vimdoc',
-                        'xml',
-                        'yaml',
-                    },
-                    floating_preview_opts = { border = 'rounded' },
-                    peek_definition_code = {
-                        ['<Leader>js'] = '@function.outer',
-                        ['<Leader>jS'] = '@class.outer',
-                    },
-                },
-            },
-            endwise = { enable = true },
-        },
         lazy = false,
-        -- ft = {
-        --     'bash',
-        --     'c',
-        --     'cpp',
-        --     'css',
-        --     'dockerfile',
-        --     'help',
-        --     'html',
-        --     'javascript',
-        --     'json',
-        --     'jsonc',
-        --     'lua',
-        --     'make',
-        --     'markdown',
-        --     'python',
-        --     'query',
-        --     'scss',
-        --     'typescript',
-        --     'vim',
-        --     'vue',
-        --     'xml',
-        --     'yaml',
-        -- },
-        -- cmd = {
-        --     'TSConfigInfo',
-        --     'TSBufDisable',
-        --     'TSBufEnable',
-        --     'TSBufToggle',
-        --     'TSDisable',
-        --     'TSEditQuery',
-        --     'TSEditQueryUserAfter',
-        --     'TSEnable',
-        --     'TSInstall',
-        --     'TSInstallInfo',
-        --     'TSInstallFromGrammar',
-        --     'TSInstallSync',
-        --     'TSModuleInfo',
-        --     'TSToggle',
-        --     'TSUninstall',
-        --     'TSUpdate',
-        --     'TSUpdateSync',
-        -- },
     },
 
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function(opts)
+            require('nvim-treesitter-textobjects').setup(opts)
+
+            -- select
+            local tsselect =
+                require('nvim-treesitter-textobjects.select').select_textobject
+            local select_keymaps = {
+                {
+                    lhs = 'af',
+                    rhs = '@function.outer',
+                    desc = 'Select outer function',
+                },
+                {
+                    lhs = 'if',
+                    rhs = '@function.inner',
+                    desc = 'Select inner function',
+                },
+                {
+                    lhs = 'ac',
+                    rhs = '@class.outer',
+                    desc = 'Select outer class',
+                },
+                {
+                    lhs = 'ic',
+                    rhs = '@class.inner',
+                    desc = 'Select inner class',
+                },
+            }
+
+            for _, keymap in ipairs(select_keymaps) do
+                vim.keymap.set({ 'x', 'o' }, keymap.lhs, function()
+                    tsselect(keymap.rhs, 'textobjects')
+                end, { desc = keymap.desc })
+            end
+
+            -- move
+            local tsmove = require 'nvim-treesitter-textobjects.move'
+            local move_keymaps = {
+                {
+                    type = 'goto_next_start',
+                    lhs = ']f',
+                    rhs = '@function.outer',
+                    desc = 'Go to the start of the next function',
+                },
+                {
+                    type = 'goto_next_start',
+                    lhs = ']c',
+                    rhs = '@class.outer',
+                    desc = 'Go to the start of the next class',
+                },
+                {
+                    type = 'goto_next_end',
+                    lhs = ']F',
+                    rhs = '@function.outer',
+                    desc = 'Go to the end of the next function',
+                },
+                {
+                    type = 'goto_next_end',
+                    lhs = ']C',
+                    rhs = '@class.outer',
+                    desc = 'Go to the end of the next class',
+                },
+                {
+                    type = 'goto_previous_start',
+                    lhs = '[f',
+                    rhs = '@function.outer',
+                    desc = 'Go to the start of the previous function',
+                },
+                {
+                    type = 'goto_previous_start',
+                    lhs = '[c',
+                    rhs = '@class.outer',
+                    desc = 'Go to the start of the previous class',
+                },
+                {
+                    type = 'goto_previous_end',
+                    lhs = '[F',
+                    rhs = '@function.outer',
+                    desc = 'Go to the end of the previous function',
+                },
+                {
+                    type = 'goto_previous_end',
+                    lhs = '[C',
+                    rhs = '@class.outer',
+                    desc = 'Go to the end of the previous class',
+                },
+            }
+
+            for _, keymap in ipairs(move_keymaps) do
+                vim.keymap.set({ 'n', 'x', 'o' }, keymap.lhs, function()
+                    tsmove[keymap.type](keymap.rhs, 'textobjects')
+                end, { desc = keymap.desc })
+            end
+        end,
+        otps = {
+            select = {
+                selection_modes = {
+                    ['@function.inner'] = 'v',
+                    ['@function.outer'] = 'V',
+                    ['@class.inner'] = 'v',
+                    ['@class.outer'] = 'V',
+                },
+                include_surrounding_whitespace = true,
+            },
+            lsp_interop = {
+                floating_preview_opts = { border = 'rounded' },
+            },
+        },
         ft = {
             -- all disabled: css, json, jsonc, make, query, scss, vimdoc, yaml
             --               dockerfile
